9bba296de321653842d4f3bb0c82d9ff42e631df
Rafa <rafaelruizbernal@gmail.com>
2019-01-31 13:22:31 +0000

    setState and if statements added

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,15 +3,37 @@ import ReactDOM from 'react-dom';
 // import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // this is the only time we do direct assignment to this.state
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        //We called setState
+        this.setState ({ lat: position.coords.latitude });
+      },
+      err => {
+        this.setState ({ errorMessage: err.message });
+      }
+    );
+  }
+
+  // React says we have to define render!!
   render () {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return <div>Latitude: </div>
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return <div>Loading!</div>
+      
   }
 };
-
 
 
 ReactDOM.render (<App />, document.querySelector('#root'))
