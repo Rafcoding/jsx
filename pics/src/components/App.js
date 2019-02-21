abf844de6af629361cd66ddc3f8085c531ef2b92
Rafa <rafaelruizbernal@gmail.com>
2019-02-21 20:07:07 +0000

    style added to the searchbar

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-
+import SearchBar from './SearchBar';
 // functional component
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="ui container" style={{ marginTop: '10px' }}>
+      <SearchBar />
+    </div>
+  );
 };
 
 export default App;
