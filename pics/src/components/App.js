44a61f41e649ffe10653b726d4669f4918f5296d
Rafa <rafaelruizbernal@gmail.com>
2019-02-22 12:56:27 +0000

    before fetching data from API

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import SearchBar from './SearchBar';
-// functional component
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: '10px' }}>
-      <SearchBar />
-    </div>
-  );
+// functional component const App = () => {
+
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmitOrSimilarNameWeWant={this.onSearchSubmit} />
+      </div>
+    );
+  }
 };
 
 export default App;
