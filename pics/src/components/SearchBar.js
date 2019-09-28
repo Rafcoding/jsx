7c4c89291fe5d9958220792a8aa0a884c101b8b2
Rafa <rafaelruizbernal@gmail.com>
2019-09-28 09:18:47 +0100

    commit after fixing error

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -10,7 +10,7 @@ class SearchBar extends React.Component {
     this.props.onSubmitOrSimilarNameWeWant(this.state.term);
   };
 
-  
+  // new line added after fixing issue on gitbhub
 
   render() {
     return (
